70332f2236c878d4d9250b5d0c16c0e248122434
freek <freek@spatie.be>
2018-12-27 10:24:04 +0100

    remove dead code

--- config/searchable.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'search_result_class' => \Spatie\Searchable\SearchResult::class,
-];
--- src/SearchServiceProvider.php
+++ src/SearchServiceProvider.php
@@ -6,19 +6,8 @@ use Illuminate\Support\ServiceProvider;
 
 class SearchServiceProvider extends ServiceProvider
 {
-    public function boot()
-    {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/skeleton.php' => config_path('skeleton.php'),
-            ], 'config');
-        }
-    }
-
     public function register()
     {
-//        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'skeleton');
-
         $this->app->singleton(Search::class);
     }
 }

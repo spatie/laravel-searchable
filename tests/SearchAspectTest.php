c04d5902ae3839ce1bec834215f196f1fdaeb8e9
Freek Van der Herten <freek@spatie.be>
2026-02-21 22:53:08 +0100

    Add Laravel 13 support (#155)
    * Bump dependencies for Laravel 13
    
    * Update GitHub Actions for Laravel 13
    
    * Add PHP 8.5 support, fix coverage warnings, ensure Pest 4/PHPUnit 12
    
    * Clean up CI workflow for L13 support
    
    * Fix CI workflow GitHub Actions syntax
    
    * Convert @test to #[Test] for PHPUnit 12
    
    * Fix styling
    
    ---------
    
    Co-authored-by: Shift <shift@laravelshift.com>
    Co-authored-by: freekmurze <freekmurze@users.noreply.github.com>

--- tests/SearchAspectTest.php
+++ tests/SearchAspectTest.php
@@ -2,11 +2,12 @@
 
 namespace Spatie\Searchable\Tests;
 
+use PHPUnit\Framework\Attributes\Test;
 use Spatie\Searchable\Tests\stubs\CustomNameSearchAspect;
 
 class SearchAspectTest extends TestCase
 {
-    /** @test */
+    #[Test]
     public function it_has_a_type()
     {
         $searchAspect = new CustomNameSearchAspect();
@@ -14,7 +15,7 @@ class SearchAspectTest extends TestCase
         $this->assertEquals('custom_names', $searchAspect->getType());
     }
 
-    /** @test */
+    #[Test]
     public function the_type_can_be_customized()
     {
         $searchAspectWithCustomName = new class extends CustomNameSearchAspect {
--- tests/SearchResultTest.php
+++ tests/SearchResultTest.php
@@ -2,12 +2,13 @@
 
 namespace Spatie\Searchable\Tests;
 
+use PHPUnit\Framework\Attributes\Test;
 use Spatie\Searchable\Searchable;
 use Spatie\Searchable\SearchResult;
 
 class SearchResultTest extends TestCase
 {
-    /** @test */
+    #[Test]
     public function it_can_store_a_search_result()
     {
         $searchable = new class implements Searchable {
